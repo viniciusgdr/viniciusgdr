55a8b2904caf5b3c81d1bcda915fd0bb5a399bd9
Vinicius Silva <viniciusvinisilva1@outlook.com>
2023-11-25 13:51:09 -0300

    fix: scroll page

--- src/components/Bulb.tsx
+++ src/components/Bulb.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const Bulb = () => {
-  return <div className='absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[280px] xl:w-[268px]'>
+  return <div className='absolute -left-36 bottom-7 rotate-12 mix-blend-color-dodge animate-pulse duration-75 z-10 w-[280px] xl:w-[268px]'>
     <Image src={'/bulb.png'} width={260} height={200} alt='Bulb' className='h-full w-full' />
   </div>;
 };
--- src/components/Circles.tsx
+++ src/components/Circles.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const Circles = () => {
-  return <div className='w-[200px] xl:w-[300px] absolute -right-16 -bottom-2 mix-blend-color-dodge animate-pulse duration-75 z-10'>
+  return <div className='w-[200px] xl:w-[300px] absolute -right-16 -bottom-0 mix-blend-color-dodge animate-pulse duration-75 z-10'>
     <Image src={'/circles.png'} width={260} height={200} alt='Circles' className='w-full h-full' />
   </div>;
 };
