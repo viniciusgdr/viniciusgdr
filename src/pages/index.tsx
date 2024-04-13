0e033fb57f4da9337d4ddeb26b1556424710dec0
Vinicius Silva <viniciusgdr@outlook.com>
2024-04-13 14:27:56 -0300

    fix: responsivity on mobile screens page initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,8 @@ import { TypeAnimation } from "react-type-animation";
 export default function Home() {
   return (
     <div className=''>
-      <div className="w-full py-12 min-h-screen h-full  bg-black/30 md:bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+      <div className="w-full md:py-12 min-h-[750px] md:min-h-screen h-full bg-black/30 md:bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center mt-auto md:mt-32 lg:mt-0 flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
@@ -68,7 +68,7 @@ export default function Home() {
           </motion.div>
         </div>
       </div>
-      <div className='w-[1200px] h-full absolute right-0 bottom-0'>
+      <div className='w-full md:w-[1200px] h-full absolute right-0 bottom-0'>
         <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
         </div>
         <ParticlesContainer />
