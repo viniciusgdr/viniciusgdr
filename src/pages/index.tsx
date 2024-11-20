f09ccad0e2f0ee0945115820b55e7861f40a8a97
Vinicius Silva <viniciusgdr@outlook.com>
2024-11-20 20:22:53 -0300

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import ProjectsBtn from "@viniciusgdr/components/ProjectsBtn";
 import { fadeIn } from "../../variants";
 import { motion } from "framer-motion";
-import Avatar from '@viniciusgdr/components/Avatar';
-import ParticlesContainer from '@viniciusgdr/components/ParticlesContainer';
+import Avatar from "@viniciusgdr/components/Avatar";
+import ParticlesContainer from "@viniciusgdr/components/ParticlesContainer";
 import { TypeAnimation } from "react-type-animation";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=''>
+    <div className="">
       <div className="w-full md:py-12 min-h-[750px] md:min-h-screen h-full bg-black/30 md:bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center mt-auto md:mt-32 lg:mt-0 flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1
@@ -15,7 +16,8 @@ export default function Home() {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h1">
+            className="h1"
+          >
             Transformando sonhos <br /> em{" "}
             <span className="text-accent">realidade</span>
           </motion.h1>
@@ -51,8 +53,9 @@ export default function Home() {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            Tire suas ideias do papel e transforme-as em realidade com meu serviço. Desenvolvo aplicações web escalaveis e mobile com as melhores tecnologias do
-            mercado.
+            Tire suas ideias do papel e transforme-as em realidade com meu
+            serviço. Desenvolvo aplicações web escalaveis e mobile com as
+            melhores tecnologias do mercado.
           </motion.p>
           <div className="flex justify-center xl:hidden relative">
             <ProjectsBtn />
@@ -68,9 +71,13 @@ export default function Home() {
           </motion.div>
         </div>
       </div>
-      <div className='w-full md:w-[1200px] h-full absolute right-0 bottom-0'>
-        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
-        </div>
+      <div className="w-full md:w-[1200px] h-full absolute right-0 bottom-0">
+        <Image
+          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+          src="/bg-explosion.png"
+          alt=""
+          fill
+        />
         <ParticlesContainer />
         <motion.div
           variants={fadeIn("up", 0.5)}
@@ -78,19 +85,22 @@ export default function Home() {
           animate="show"
           exit="hidden"
           transition={{ duration: 1, ease: "easeInOut" }}
-          className='w-full h-full max-w-[747px] max-h-[678px] absolute md:-bottom-32 lg:bottom-0 lg:right-[8%]'>
+          className="w-full h-full max-w-[747px] max-h-[678px] absolute md:-bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
           <Avatar />
         </motion.div>
       </div>
-      <div className='hidden'>
+      <div className="hidden">
         {
           // SEO Component
         }
         <h1>Carlos Vinicius - Desenvolvedor Web e Mobile</h1>
-        <p>Carlos Vinicius - Desenvolvedor Web e Mobile - HTML, CSS, JavaScript, React, Next.js, Node.js, TypeScript, Docker, Clean Architecture, Aplicações Escaláveis, Aplicações Web, Aplicações Mobile | viniciusgdr</p>
-        <h2>
-          viniciusgdr.com
-        </h2>
+        <p>
+          Carlos Vinicius - Desenvolvedor Web e Mobile - HTML, CSS, JavaScript,
+          React, Next.js, Node.js, TypeScript, Docker, Clean Architecture,
+          Aplicações Escaláveis, Aplicações Web, Aplicações Mobile | viniciusgdr
+        </p>
+        <h2>viniciusgdr.com</h2>
       </div>
     </div>
   );
